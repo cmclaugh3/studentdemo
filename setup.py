6728270f4db1d9d85415f09ab2204f6dad7fac06
Conor McLaughlin <cfm5qc@virginia.edu>
2023-07-31 11:04:19 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='studentdemo',
+    version='0.1.0',
+    url='https://github.com/cmclaugh3/studentdemo',
+    author='Conor McLaughlin',
+    description='Package containing Python Scripts for DS5100 in-class M09',
+    packages=find_packages(),
+    license='MIT
+    install_requires=['pandas >= 1.1.3']
+)
